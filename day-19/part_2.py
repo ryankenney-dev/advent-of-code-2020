e42adc5a45afde14d52b2c6ac3e7b5ff3db1a661
Ryan Kenney <ryan@ryankenney.info>
2020-12-20 13:36:13 -0800

    Applied part 1 refactor, added debug code, and fixed the bug

--- day-19/part_2.py
+++ day-19/part_2.py
@@ -41,14 +41,46 @@ def test_all_messages(input):
 	return result
 
 def match_string(s, rules):
-	initial_thread = { 'seq_stack': [{ 'idx': 0, 'seq': [0] }] }
+	initial_thread = { 'seq_stack': [{ 'rule_id': 0, 'or_idx': 0, 'seq_idx': 0 }] }
 	threads = [initial_thread]
-	for c in range(0,len(s)):
-		char = s[c]
-		threads = match_char(char, c, rules, threads)
+	for char_idx in range(0, len(s)):
+		char = s[char_idx]
+
+		# print('-- char %s:%s --' % (char_idx, char))
+
+		threads = match_char(char, char_idx, rules, threads)
 		if len(threads) < 1:
 			return False
-	return True
+
+	for thread in threads:
+		# If the stack isn't empty, there are unmatched trailing rules
+		if len(thread['seq_stack']) == 0:
+			return True
+
+	return False
+
+EXAMPLE = {
+	'seq_stack': [
+		{ 'rule_id': 0, 'or_idx': 0, 'seq_idx': 0 },
+		{ 'rule_id': 0, 'or_idx': 0, 'seq_idx': 0 }
+	]
+}
+
+def validate_solution_history(thread, rules):
+	for solution in thread['solutions']:
+		next_rule_id = 0
+		for entry in solution['seq_stack']:
+			if next_rule_id != entry['rule_id']:
+				raise Exception('Invalid entry')
+			rule = rules[next_rule_id]
+			if 'or' in rule:
+				next_rule_id = rule['or'][entry['or_idx']]['seq'][entry['seq_idx']]
+			elif 'seq' in rule:
+				next_rule_id = rule['seq'][entry['seq_idx']]
+			else:
+				raise Exception('Invalid entry')
+		if solution['char'] != rules[next_rule_id]:
+			raise Exception('Char not matched')
 
 # Theory of operation:
 # * Maintain state as a stack of sequences
@@ -56,7 +88,6 @@ def match_string(s, rules):
 #   and in clone (fork) apply a different or clause
 # * Evaluate until all threads match/don't match this char
 def match_char(char, char_idx, rules, unsolved_threads):
-
 	solved_threads = []
 	while len(unsolved_threads) > 0:
 		thread = unsolved_threads.pop()
@@ -67,9 +98,21 @@ def match_char(char, char_idx, rules, unsolved_threads):
 			# We ran out of rules
 			continue
 
-		state = thread['seq_stack'][-1]
-		rule_id = state['seq'][state['idx']]
+		last_state = thread['seq_stack'][-1]
+		last_rule_id = last_state['rule_id']
+		last_rule = rules[last_rule_id]
+		last_seq_idx = last_state['seq_idx']
+		last_or_idx = last_state['or_idx']
+		if 'or' in last_rule:
+			rule_id = last_rule['or'][last_or_idx]['seq'][last_seq_idx]
+		elif 'seq' in last_rule:
+			rule_id = last_rule['seq'][last_seq_idx]
+		else:
+			raise Exception('Invalid entry: %s' % last_rule)
+
 		rule = rules[rule_id]
+
+		# print('Rule: %s' % rule)
 
 		# If we hit these rules twice without advancing
 		# to a new char, we're in an infinite loop.
@@ -84,8 +127,6 @@ def match_char(char, char_idx, rules, unsolved_threads):
 					continue
 			thread['last_rule_11_char_idx'] = char_idx
 
-		# print('Rule: %s' % rule)
-
 		if isinstance(rule, str):
 			# Not a match
 			if char != rule:
@@ -93,27 +134,30 @@ def match_char(char, char_idx, rules, unsolved_threads):
 				# print('Not a match: %s %s' % (char, rule))
 
 				continue
-			# Walk to next element in parent sequence (for next chat match)
-			increment_seq_stack(thread)
 
 			# print('Marking thread solved: %s' % thread)
+
+			save_solution_history(thread, char)
+
+			# Walk to next element in parent sequence (for next chat match)
+			increment_seq_stack(thread, rules)
 
 			solved_threads.append(thread)
 
 		elif 'seq' in rule:
-			thread['seq_stack'].append({ 'idx': 0, 'seq': rule['seq'] })
+			thread['seq_stack'].append({ 'rule_id': rule_id, 'or_idx': 0, 'seq_idx': 0 })
 			unsolved_threads.append(thread)
 
 		elif 'or' in rule:
 			# Duplicate (fork) thread for each or clause
 			forked_threads = []
-			for term in rule['or']:
+			for term_idx in range(0,len(rule['or'])):
 				# NOTE: Ok ok... making one extra copy that we toss out,
 				# but keeps logic simple
 				forked_thread = copy.deepcopy(thread)
 				forked_threads.append(forked_thread)
 				# Put the or'd clause at the top of the stack
-				forked_thread['seq_stack'].append({ 'idx': 0, 'seq': term['seq'] })
+				forked_thread['seq_stack'].append({ 'rule_id': rule_id, 'or_idx': term_idx , 'seq_idx': 0 })
 
 			# print('forked: %s', len(forked_threads))
 
@@ -121,21 +165,43 @@ def match_char(char, char_idx, rules, unsolved_threads):
 
 	return solved_threads
 
-def increment_seq_stack(thread):
+def increment_seq_stack(thread, rules):
 
 	# print('Incn seq stack (before): %s' % thread)
 
 	# Pop off all parent sequences we've exhausted
 	while len(thread['seq_stack']) > 0 and \
-		thread['seq_stack'][-1]['idx']+1 >= len(thread['seq_stack'][-1]['seq']):
+		top_of_stack_exhausted(thread['seq_stack'], rules):
 
 		# print('Popped stack of: %s' % thread['seq_stack'][-1])
 
 		thread['seq_stack'].pop()
 	# Increment resulting top sequence
 	if len(thread['seq_stack']) > 0:
-		thread['seq_stack'][-1]['idx'] += 1
+		thread['seq_stack'][-1]['seq_idx'] += 1
 	# If no stack entries remain (and another char is encounterd),
 	# it'll be detected on the next cycle above.
 
 	# print('Incn seq stack (after): %s' % thread)
+
+
+def top_of_stack_exhausted(stack, rules):
+	top = stack[-1]
+	rule = rules[top['rule_id']]
+	if 'seq' in rule:
+		seq = rule['seq']
+	elif 'or' in rule:
+		seq = rule['or'][top['or_idx']]['seq']
+	else:
+		raise Exception('Invalid entry: %s' % rule)
+	return top['seq_idx']+1 >= len(seq)
+
+
+USE_SOLUTION_HISTORY=False
+
+def save_solution_history(thread, char):
+	if not USE_SOLUTION_HISTORY:
+		return
+	if 'solutions' not in thread:
+		thread['solutions'] = []
+	thread['solutions'].append({ 'char':char, 'seq_stack':copy.deepcopy(thread['seq_stack']) })
