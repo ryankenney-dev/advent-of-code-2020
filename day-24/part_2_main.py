df82c3d8f8835206f04345de3f0071f01aa879ea
Ryan Kenney <ryan@ryankenney.info>
2020-12-24 00:19:37 -0800

    Copied part 1 to part 2

---
+++ day-24/part_2_main.py
@@ -0,0 +1,13 @@
+import part_2
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    message = f.read()
+
+tile_paths = part_2.parse_input(message)
+tile_states = part_2.flip_all_paths(tile_paths)
+black_tile_count = part_2.count_black_tiles(tile_states)
+
+print("")
+print("[[[ Result: %s ]]]" % black_tile_count)
+print("")
